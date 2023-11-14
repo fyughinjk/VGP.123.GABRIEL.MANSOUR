c15ce537f378ced10fa40b98297311f36a8b942e
fyughinjk <gabrielmansour03@gmail.com>
2023-11-14 13:20:19 -0500

    Lab 4

--- Assets/Scripts/Mario/PlayerController.cs
+++ Assets/Scripts/Mario/PlayerController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    // Component References
+    //Component References
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator anim;
 
-    // Movement Variables
+
+    //Movement variables
     public float speed = 5.0f;
     public float jumpForce = 300.0f;
 
-    //GroundedCheck 
+    //GroundCheck stuff
     public bool isGrounded;
     public Transform groundCheck;
     public LayerMask isGroundLayer;
@@ -23,36 +24,31 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Getting Our Component References
+        //Getting our component references
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-
-        //Checking variables for Dirty Data
-        if (rb == null) Debug.Log("No Rigidbody Reference");
+        //checking variables for dirty data
+        if (rb == null) Debug.Log("No RigidBody Reference");
         if (sr == null) Debug.Log("No Sprite Renderer Reference");
-        if (anim == null) Debug.Log("No Animator Reference");
-
-        if (groundCheckRadius <=0)
+        if (anim == null) Debug.Log("No animator reference");
+        if (groundCheckRadius <= 0)
         {
             groundCheckRadius = 0.02f;
-            Debug.Log("groundCheck set to default value");
+            Debug.Log("groundcheck set to default value");
         }
-
         if (speed <= 0)
         {
             speed = 5.0f;
             Debug.Log("speed set to default value");
         }
-
         if (jumpForce <= 0)
         {
             jumpForce = 300.0f;
-            Debug.Log("jumpForce set to default value");
+            Debug.Log("jumpforce set to default value");
         }
-
-        if(groundCheck == null)
+        if (groundCheck == null)
         {
             GameObject obj = new GameObject();
             obj.transform.SetParent(gameObject.transform);
@@ -65,19 +61,82 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AnimatorClipInfo[] curPlayingClips = anim.GetCurrentAnimatorClipInfo(0);
         float hInput = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+        if (isGrounded) rb.gravityScale = 1;
 
-        if(isGrounded && Input.GetButtonDown("Jump"))
+
+        if (curPlayingClips.Length > 0)
+        {
+            if (curPlayingClips[0].clip.name == "Fire")
+                rb.velocity = Vector2.zero;
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+        }
+
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector2.up * jumpForce);
         }
 
-        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
-        rb.velocity = moveDirection;
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+            anim.SetTrigger("JumpAttack");
+
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Fire");
+        }
 
         anim.SetFloat("hInput", Mathf.Abs(hInput));
         anim.SetBool("isGrounded", isGrounded);
+
+        //flip functionality
+        if (hInput != 0) sr.flipX = (hInput < 0);
+    }
+
+    public void IncreaseGravity()
+    {
+        rb.gravityScale = 10;
+    }
+
+    //called the frame that the collision happens
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    //called the frame that the collision exits
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+
+    //called on the second frame while in the collision, and continously called while you remain in the collider.
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
     }
 }
